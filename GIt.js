23841fc4924fde8eae144b1e31a90c379e8b78f5
Rafael Villarreal <rvillarreal416@gmail.com>
2017-01-29 23:13:39 -0500

    S

--- GIt.js
+++ GIt.js
@@ -1,9 +1,11 @@
-var comm = 'git add -A; git commit -m';
-var upload = 'git push -u origin master';
-var ping = 'if netcat -z google.com 80; then echo "1"; else echo "0"; fi';
-var shell = require('shelljs/global');
-var prompt = require('prompt');
+var comm = 'git add -A; git commit -m';  //Here we save the commit comand into a variable
+var upload = 'git push -u origin master'; //Here we save the push comand into a variable
+var ping = 'if netcat -z google.com 80; then echo "1"; else echo "0"; fi'; //This is the ping that verify the internet connection
+var shell = require('shelljs/global'); //the shelljs module to write shellscriptinng with nodejs
+var prompt = require('prompt'); //the prompt module to show data on terminal
 
+
+// with this conditional we validate if theres a conection or not and show a message acording to the status
 
 if (exec(ping) == 1) {
 	console.log('You are online');
@@ -24,13 +26,8 @@ function local () {
 
 function cloud () {
 	prompt.start();
+	local();
 	var ask = '¿Quieres enviarlo a GitHub?, (S/N)';
-
-	prompt.get(['comentario'], function (err, result){
-		if (err) {return onErr(err);}
-		exec(comm + result.comentario);
-	})
-
 	prompt.get([ask], function (err, result) {
 	  if (err) { return onErr(err); }
 	  if (result.ask == S || s) {
@@ -41,9 +38,10 @@ function cloud () {
 	});
 }
 
-
+// This is the function  that deal with errors 
+//  Esta es la funcion que se encarga de manejar los errores
 function onErr(err) {
-  console.log(err);
+  console.log(err); //Aqui le decimos que imprima el error en la consola
   return 1;
 }
 
